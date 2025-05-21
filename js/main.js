3b0f7fcec5c82b930daf92c1f835d20aaa939ec7
Ari Rahmad Afrizal <arirahmad004@gmail.com>
2025-05-22 01:12:45 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+  const sound = document.getElementById("bloomSound");
+  sound.volume = 1;
+  sound.muted = false;
+  sound.loop = true; // <-- supaya looping otomatis
+
+  sound.play().catch((e) => {
+    console.log("Autoplay suara gagal, browser mungkin butuh interaksi:", e);
+  });
+
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = "I LOVE U".split("");
+    const titleElement = document.getElementById("title");
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300);
+      } else {
+        const naswaElement = document.createElement("div");
+        naswaElement.textContent = "naswa";
+        naswaElement.style.marginTop = "10px";
+        titleElement.parentNode.insertBefore(
+          naswaElement,
+          titleElement.nextSibling
+        );
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
